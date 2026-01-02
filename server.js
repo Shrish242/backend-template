2827fcdf896893e98d289033434a6f53a9b1433a
Shrish Dhakal <shrishd242@gmail.com>
2026-01-02 14:28:22 +0545

    test

--- server.js
+++ server.js
@@ -41,12 +41,28 @@ app.use(cors({
 }));
 
 // 👇 REQUIRED for Vercel
-app.options("*", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://ecommerce-manage-frontend-ewq7.vercel.app");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  return res.status(200).end();
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://ecommerce-manage-frontend-ewq7.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,DELETE,OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
 });
+
+app.use(express.json());
 // ==================== RATE LIMITERS ====================
 
 const authLimiter = rateLimit({
